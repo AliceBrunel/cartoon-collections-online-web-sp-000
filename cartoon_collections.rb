fc4d97328d42b56db189e509ae4e7ce8b452f18f
Alice Brunel <alc.brunel@gmail.com>
2019-07-08 08:42:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese = array.include?(cheese_type)
+  cheese = array.include?(cheese_types)
   cheese
 end
